64ecae1a190921b878ccb3729a76b915f83a1748
Radosław Rocławski <r.roclawski@ankara.pl>
2018-02-14 07:04:31 +0100

    Created files structure expected to exist in the project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/styles.css';
+
+export default class extends React.Component {
+
+    render() {
+
+        return  <React.Fragment>
+                    Project template & files structure READY.
+                </React.Fragment>
+    }
+}
+

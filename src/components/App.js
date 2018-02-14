7c7803fd42d96d9c41f8fd21ac45188d445ebbdd
Radosław Rocławski <r.roclawski@ankara.pl>
2018-02-14 07:54:45 +0100

    Created todo list in main app file *App.js*, tested state accessibility

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,17 @@ import '../css/styles.css';
 
 export default class extends React.Component {
 
+    state = [
+                { name: 'Buy a beer', isFinished: false },
+                { name: 'Wash my armpits', isFinished: false },
+                { name: 'Shave bikini surroundings', isFinished: false },
+                { name: 'Look for mistakes in LOTR Trilogy', isFinished: false }
+    ];
+
     render() {
 
         return  <React.Fragment>
-                    Project template & files structure READY.
+            { this.state[0].name }
                 </React.Fragment>
     }
 }

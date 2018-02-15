28dca4d918579d11738d22b630c5e26420ad6dfb
Radosław Rocławski <r.roclawski@ankara.pl>
2018-02-15 20:43:35 +0100

    Added input state to AddTodo component. Input state refreshes itself after inputing some txt

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 export default class extends React.Component {
+
+    state = {
+        currentInputValue: ''
+    };
+
+    onInputChange = (e) => {
+        this.setState({
+            currentInputValue: e.target.value
+        })
+    };
+
     render() {
+
         return (<React.Fragment>
-            <input type="text" />
+            <input type="text"
+                onChange={this.onInputChange}/>
             <button>Add</button>
         </React.Fragment>)
+
     }
 };
